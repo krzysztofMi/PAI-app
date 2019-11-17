fbdf28df02098cd26d66f6a6cdfa349eba5ca409
unknown <krzysztofmichalski987@gmail.com>
2019-11-17 12:54:34 +0100

    add style to register page

--- views/register.php
+++ views/register.php
@@ -3,35 +3,31 @@
     <head>
         <meta charset="UTF-8">
         <title>Title</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="../../resource/style.css">
+        <link rel="stylesheet" type="text/css" href="../resource/style/style.css">
+        <link rel="stylesheet" type="text/css" href="../resource/style/logRegStyle.css">
     </head>
     <body>
         <?php include "fragment/header.php" ?>
-        <div>
-            <h1>Rejestracja</h1>
-        </div>
-        <form>
-            Login:
-            <input type="text">
-            Email:
-            <input type="email">
-            Hasło:
-            <input type="password">
-            Powtórz hasło:
-            <input type="password">
-            <button type="button">Zarejestruj się</button>
-        </form>
-        <div>
-            <a href="index.php"><button>Powrót</button></a>
-        </div>
-        <div>
-            <a href="login.php"><button>Nie masz konta? Zaloguj się!</button></a>
+        <div class="verticalContainer">
+            <div class="underline-header">
+                <h1>Rejestracja</h1>
+            </div>
+            <form>
+                Login:
+                <input type="text">
+                Email:
+                <input type="email">
+                Hasło:
+                <input type="password">
+                Powtórz hasło:
+                <input type="password">
+                <button type="button" class="button-yellow">Zarejestruj się</button>
+            </form>
+            <a style="width: 50%;" href="login.php">
+                <button class="button-yellow">Nie masz konta? Zaloguj się!</button>
+            </a>
         </div>
         <?php include "fragment/footer.php" ?>
     </body>
 </html>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
